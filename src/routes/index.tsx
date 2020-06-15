f072b751c1fe1e8e495edf0cf9a00faca8219da7
Davi Wendt <daviwendt@gmail.com>
2020-06-15 00:17:39 -0300

    Add Reset Password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,13 +7,15 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import DashBoard from '../pages/DashBoard';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
     <Route path="/dashboard" component={DashBoard} isPrivate />
-    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/forgot_password" component={ForgotPassword} />
+    <Route path="/reset_password" component={ResetPassword} />
   </Switch>
 );
 
